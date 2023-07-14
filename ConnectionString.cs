4d2a11e813166dfce17cec8a11ab83df17f755a9
kevin-mbon <mbonikevin01@gmail.com>
2023-07-14 13:12:03 +0300

    Add project files.

---
+++ ConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace College_Management_System
+{
+    class ConnectionString
+    {
+        public string DBConn =/* @"Data Source=DESKTOP-2LLV29I\MSSQLSERVER01;Initial Catalog=SimpleAccount;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";*/
+        @"Data Source=.;Initial Catalog=SimpleAccount;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+       //Data Source=SOLA;Initial Catalog=CMS_DB.MDF;Integrated Security=True;Connect Timeout=30
+        //Data Source=SOLA\SOLA;Initial Catalog=CMS_DB.MDF;Integrated Security=True
+    }
+}
---
+++ DataSet1.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleaccountingSys
+{
+
+
+    public partial class DataSet1
+    {
+    }
+}
+namespace SimpleaccountingSys {
+    
+    
+    public partial class DataSet1 {
+    }
+}
